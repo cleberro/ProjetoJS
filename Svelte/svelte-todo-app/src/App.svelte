de9b59e246dd7549944c1182ab378b1e6406a7ca
cleber <cleberro@hotmail.com>
2020-05-24 06:57:20 -0300

    Subir pasta Svelte

---
+++ Svelte/svelte-todo-app/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Task from "./Tasks/Task.svelte";
+	const projectName = "To Do App";
+	const src="https://avatars2.githubusercontent.com/u/23617963?s=40&v=4";
+
+</script>
+
+<section class="section">
+	<div class="container">
+		<figure class="image is-64x64">
+			<img src={src} alt="Svelte3"/>
+		</figure>
+		<h1 class="title">{projectName}</h1>
+		<p class="subtitle">
+			My first website made with <strong>Svelte!</strong>
+		</p>
+	</div>
+	<Task />
+</section>
---
+++ Svelte/svelte-todo-app/src/Tasks/Task.svelte
@@ -0,0 +1,49 @@
+<script>
+    let NewDescription = "";
+    let validDescription = false;
+    $: validDescription = NewDescription.trim().length >= 5; // declaração reativa
+</script>
+
+<div class="container">
+    <div class="notification">
+        <!-- Form -->
+        <div class="field is-horizontal">
+            <div class="field-label is-normal">
+                <label class="label">Task</label>
+            </div>
+            <div class="field-body">
+                <div class="field">
+                    <p class="control is-expanded has-icon-left">
+                        <input id="NewDescriptionId" bind:value={NewDescription} class="input" type="text" placeholder="Enter new task..." />
+                        <span class="icon is-small is-left">
+                            <i class="fas fa-tasks" />
+                        </span>
+                    </p>
+                </div>
+                <!-- Buttons -->
+                <div class="field is-grouped">
+                    <div class="control">
+                        <button class="button is-normal">New</button>
+                    </div>
+                    <div class="control">
+                        <button class="button is-info" disabled={ !validDescription }>
+                            <span class="icon is-small">
+                                <i class="fas fa-check" />
+                            </span>
+                            <span>Save</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Binding -->
+        <div class="field is-horizontal adding-record">
+            <div class="field label">New record:</div>
+            <div class="field body">
+                <div class="field">
+                    <div class="control">{NewDescription}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
